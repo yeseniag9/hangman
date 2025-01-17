041d55ad1e39546e6dc8e8b7fde3736a9e386149
Yesenia <yeseniaaag9@gmail.com>
2025-01-16 23:46:56 -0600

    main idea of game

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ guesses = ''
 turns = 8
 
 while turns > 0:
+    failed = 0
+
+    for char in word:
+
+        if char in guesses:
+            print(char, end=" ")
+
+        else:
+            print("_")
+            failed += 1
+
+    if failed == 0:
+        print("You Win")
+        print("The word is: ", word)
+        break
+
+    print()
+    guess = input("guess a character:")
+
+    guesses += guess
+
+    if guess not in word:
+
+        turns -= 1
+        print("Wrong")
+        print("You have", + turns, 'more guesses')
+
+        if turns == 0:
+            print("You Loose")
