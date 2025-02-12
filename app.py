ff67327e4b0b2a7cdf570f9367d1bfaf0338c58e
Yesenia <yeseniaaag9@gmail.com>
2025-02-11 22:12:03 -0600

    removed test files from flask app and cleaned up code for project

--- app.py
+++ app.py
@@ -3,22 +3,6 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hangman.html",
-        name=name,
-    )
-
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
