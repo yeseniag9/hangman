57e41f7bf17a7e7ba1ceb9e68e1b8d3312707c60
Yesenia <yeseniaaag9@gmail.com>
2025-02-11 21:25:45 -0600

    created hangman.html template and rendered page in app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hangman.html",
+        name=name,
+    )
