a1ba3953b91137267a205fcff18f38f763df84dc
Yesenia <yeseniaaag9@gmail.com>
2025-03-27 18:18:16 -0500

    all letters configured in js

--- static/main.js
+++ static/main.js
@@ -11,3 +11,122 @@ function B() {
 	let B = document.getElementById("B");
 	B.style.color = "green";
 }
+
+function C() {
+	let C = document.getElementById("C");
+	C.style.color = "green";
+}
+
+function D() {
+	let D = document.getElementById("D");
+	D.style.color = "green";
+}
+
+function E() {
+	let E = document.getElementById("E");
+	E.style.color = "green";
+}
+
+function F() {
+	let F = document.getElementById("F");
+	F.style.color = "green";
+}
+
+function G() {
+	let G = document.getElementById("G");
+	G.style.color = "green";
+}
+
+function H() {
+	let H = document.getElementById("H");
+	H.style.color = "green";
+}
+
+function I() {
+	let I = document.getElementById("I");
+	I.style.color = "green";
+}
+
+function J() {
+	let J = document.getElementById("J");
+	J.style.color = "green";
+}
+
+function K() {
+	let K = document.getElementById("K");
+	K.style.color = "green";
+}
+
+function L() {
+	let L = document.getElementById("L");
+	L.style.color = "green";
+}
+
+function M() {
+	let M = document.getElementById("M");
+	M.style.color = "green";
+}
+
+function N() {
+	let N = document.getElementById("N");
+	N.style.color = "green";
+}
+
+function O() {
+	let O = document.getElementById("O");
+	O.style.color = "green";
+}
+
+function P() {
+	let P = document.getElementById("P");
+	P.style.color = "green";
+}
+
+function Q() {
+	let Q = document.getElementById("Q");
+	Q.style.color = "green";
+}
+
+function R() {
+	let R = document.getElementById("R");
+	R.style.color = "green";
+}
+
+function S() {
+	let S = document.getElementById("S");
+	S.style.color = "green";
+}
+
+function T() {
+	let T = document.getElementById("T");
+	T.style.color = "green";
+}
+
+function U() {
+	let U = document.getElementById("U");
+	U.style.color = "green";
+}
+
+function V() {
+	let V = document.getElementById("V");
+	V.style.color = "green";
+}
+
+function W() {
+	let W = document.getElementById("W");
+	W.style.color = "green";
+}
+
+function X() {
+	let X = document.getElementById("X");
+	X.style.color = "green";
+}
+
+function Y() {
+	let Y = document.getElementById("Y");
+	Y.style.color = "green";
+}
+function Z() {
+	let Z = document.getElementById("Z");
+	Z.style.color = "green";
+}
