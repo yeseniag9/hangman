af020017af2dcdaa5598a87d6ab9e069b7af78e2
Yesenia <yeseniaaag9@gmail.com>
2025-05-05 16:59:32 -0500

    popup

--- static/main.js
+++ static/main.js
@@ -1,3 +1,14 @@
 function playGame() {
 	location.href='game';
 }
+
+$ = function(id) {
+	return document.getElementById(id);
+  }
+  
+  var show = function(id) {
+	  $(id).style.display ='block';
+  }
+  var hide = function(id) {
+	  $(id).style.display ='none';
+  }  
