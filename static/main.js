31fc01f5ece3e11c8f4a3a9379885da6798fbe97
Yesenia <yeseniaaag9@gmail.com>
2025-05-05 17:22:36 -0500

    removed second popup

--- static/main.js
+++ static/main.js
@@ -4,11 +4,12 @@ function playGame() {
 
 $ = function(id) {
 	return document.getElementById(id);
-  }
-  
-  var show = function(id) {
-	  $(id).style.display ='block';
-  }
-  var hide = function(id) {
-	  $(id).style.display ='none';
-  }  
+}
+
+var show = function(id) {
+	$(id).style.display ='block';
+}
+
+var hide = function(id) {
+	$(id).style.display ='none';
+}  
