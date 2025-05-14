975a0fef3f73227d43e47b3d312e6b1933736ac3
Yesenia <yeseniaaag9@gmail.com>
2025-05-13 20:39:17 -0500

    every letter highlights when matching with input

--- static/main.js
+++ static/main.js
@@ -16,12 +16,98 @@ function letterPicked() {
     var input = document.getElementById("userInput").value;
     alert(input);
 
-	var letterA = document.getElementById("A");
+	var A = document.getElementById("A");
+	var B = document.getElementById("B");
+	var C = document.getElementById("C");
+	var D = document.getElementById("D");
+	var E = document.getElementById("E");
+	var F = document.getElementById("F");
+	var G = document.getElementById("G");
+	var H = document.getElementById("H");
+	var I = document.getElementById("I");
+	var J = document.getElementById("J");
+	var K = document.getElementById("K");
+	var L = document.getElementById("L");
+	var M = document.getElementById("M");
+	var N = document.getElementById("N");
+	var O = document.getElementById("O");
+	var P = document.getElementById("P");
+	var Q = document.getElementById("Q");
+	var R = document.getElementById("R");
+	var S = document.getElementById("S");
+	var T = document.getElementById("T");
+	var U = document.getElementById("U");
+	var V = document.getElementById("V");
+	var W = document.getElementById("W");
+	var X = document.getElementById("X");
+	var Y = document.getElementById("Y");
+	var Z = document.getElementById("Z");
+
+
 	if (input == "A") {
-		letterA.style.color = "green";
-	} else {
-		letterA.style.color = "red";
+		A.style.color = "green";
+	} else if (input == "B" ) {
+		B.style.color = "green";
+	} else if (input == "C" ) {
+		C.style.color = "green";
+	} else if (input == "D" ) {
+		D.style.color = "green";
+	} else if (input == "E" ) {
+		E.style.color = "green";
+	} else if (input == "F" ) {
+		F.style.color = "green";
+	} else if (input == "G" ) {
+		G.style.color = "green";
+	} else if (input == "H" ) {
+		H.style.color = "green";
+	} else if (input == "I" ) {
+		I.style.color = "green";
+	} else if (input == "J" ) {
+		J.style.color = "green";
+	} else if (input == "K" ) {
+		K.style.color = "green";
+	} else if (input == "L" ) {
+		L.style.color = "green";
+	} else if (input == "M" ) {
+		M.style.color = "green";
+	} else if (input == "N" ) {
+		N.style.color = "green";
+	} else if (input == "O" ) {
+		O.style.color = "green";
+	} else if (input == "P" ) {
+		P.style.color = "green";
+	} else if (input == "Q" ) {
+		Q.style.color = "green";
+	} else if (input == "R" ) {
+		R.style.color = "green";
+	} else if (input == "S" ) {
+		S.style.color = "green";
+	} else if (input == "T" ) {
+		T.style.color = "green";
+	} else if (input == "U" ) {
+		U.style.color = "green";
+	} else if (input == "V" ) {
+		V.style.color = "green";
+	} else if (input == "W" ) {
+		W.style.color = "green";
+	} else if (input == "X" ) {
+		X.style.color = "green";
+	} else if (input == "Y" ) {
+		Y.style.color = "green";
+	} else if (input == "Z" ) {
+		Z.style.color = "green";
 	}
+
+
+
+
+
+
+
+
+
+
+
 
 	console.log(letterA);
 	console.log(input);
