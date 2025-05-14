f69f946a01836eec5f5a11d20de056ec842f1785
Yesenia <yeseniaaag9@gmail.com>
2025-05-13 20:11:42 -0500

    form input displayed in alert

--- static/main.js
+++ static/main.js
@@ -11,3 +11,8 @@ function hideWin() {
 	let hideWin = document.getElementById("win-popup");
 	hideWin.style.display ='none';
 }
+
+function othername() {
+    var input = document.getElementById("userInput").value;
+    alert(input);
+}
