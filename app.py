d9cf7c26b82a25e141777523a5e631186ade4705
Yesenia <yeseniaaag9@gmail.com>
2025-09-20 17:55:06 -0500

    uppercased random word

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def game():
             "jubilant", "keystone", "labyrinth", "moonbeam", "novella", "obsidian", "phenomenon", "quintessence",  
             "resonance", "silk", "tempest", "utopia", "verdant", "willow", "xylophonist", "yesteryear", "zealous"]
     word = random.choice(words) 
+    word = word.upper()
 
     randomWord = []
     underscores = ""
